6cb1ee7db1d8c79cdf3b5cf0a94b3059120e8efb
Sébastien Galtier <code@galtier.org>
2026-08-20 11:30:56 +0200

    Adds MCP tools to read and edit a document

--- mcp_server.py
+++ mcp_server.py
@@ -1,3 +1,4 @@
+from pydantic import Field
 from mcp.server.fastmcp import FastMCP
 
 mcp = FastMCP("DocumentMCP", log_level="ERROR")
@@ -12,8 +13,32 @@ docs = {
     "spec.txt": "These specifications define the technical requirements for the equipment.",
 }
 
-# TODO: Write a tool to read a doc
-# TODO: Write a tool to edit a doc
+@mcp.tool(
+    name="read_doc_contents",
+    description="Read the contents of a document and return it as a string."
+)
+def read_document(
+    doc_id: str = Field(description="Id of the document to read.")
+):
+    if (doc_id not in docs):
+        raise ValueError(f"Document with id '{doc_id}' not found.")
+    
+    return docs[doc_id]
+
+@mcp.tool(
+    name="edit_document",
+    description="Edit a document by replacing a string in the document's content with a new string."
+)
+def edit_document(
+    doc_id: str = Field(description="Id of the document to edit."),
+    old_string: str = Field(description="The text to replace. Must match exactly, including whitespace."),
+    new_string: str = Field(description="The new text to insert in place of the old text.")
+):
+    if (doc_id not in docs):
+        raise ValueError(f"Document with id '{doc_id}' not found.")
+    
+    docs[doc_id] = docs[doc_id].replace(old_string, new_string)    
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
